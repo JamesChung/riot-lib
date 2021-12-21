70151df399761f5bef043dc23f27ec7ad09c23f1
James Chung <james@chung.contact>
2021-12-21 02:30:26 -0600

    Initial Commit

--- src/league/match_v5.rs
+++ src/league/match_v5.rs
@@ -257,6 +257,201 @@ pub struct Tower {
     pub kills: i64,
 }
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MatchTimeline {
+    pub metadata: Metadata,
+    pub info: TimelineInfo,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TimelineInfo {
+    pub frame_interval: i64,
+    pub frames: Vec<Frame>,
+    pub game_id: i64,
+    pub participants: Vec<TimelineParticipant>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Frame {
+    pub events: Vec<Event>,
+    pub participant_frames: ParticipantFrames,
+    pub timestamp: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Event {
+    pub real_timestamp: Option<i64>,
+    pub timestamp: i64,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub level: Option<i64>,
+    pub participant_id: Option<i64>,
+    pub item_id: Option<i64>,
+    pub level_up_type: Option<String>,
+    pub skill_slot: Option<i64>,
+    #[serde(default)]
+    pub assisting_participant_ids: Vec<i64>,
+    pub bounty: Option<i64>,
+    pub kill_streak_length: Option<i64>,
+    pub killer_id: Option<i64>,
+    pub position: Option<Position>,
+    #[serde(default)]
+    pub victim_damage_dealt: Vec<VictimDamageDealt>,
+    #[serde(default)]
+    pub victim_damage_received: Vec<VictimDamageReceived>,
+    pub victim_id: Option<i64>,
+    pub kill_type: Option<String>,
+    pub multi_kill_length: Option<i64>,
+    pub after_id: Option<i64>,
+    pub before_id: Option<i64>,
+    pub gold_gain: Option<i64>,
+    pub creator_id: Option<i64>,
+    pub ward_type: Option<String>,
+    pub building_type: Option<String>,
+    pub lane_type: Option<String>,
+    pub team_id: Option<i64>,
+    pub tower_type: Option<String>,
+    pub game_id: Option<i64>,
+    pub winning_team: Option<i64>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Position {
+    pub x: i64,
+    pub y: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct VictimDamageDealt {
+    pub basic: bool,
+    pub magic_damage: i64,
+    pub name: String,
+    pub participant_id: i64,
+    pub physical_damage: i64,
+    pub spell_name: String,
+    pub spell_slot: i64,
+    pub true_damage: i64,
+    #[serde(rename = "type")]
+    pub type_field: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct VictimDamageReceived {
+    pub basic: bool,
+    pub magic_damage: i64,
+    pub name: String,
+    pub participant_id: i64,
+    pub physical_damage: i64,
+    pub spell_name: String,
+    pub spell_slot: i64,
+    pub true_damage: i64,
+    #[serde(rename = "type")]
+    pub type_field: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ParticipantFrames {
+    #[serde(rename = "1")]
+    pub frame1: ParticipantFrame,
+    #[serde(rename = "2")]
+    pub frame2: ParticipantFrame,
+    #[serde(rename = "3")]
+    pub frame3: ParticipantFrame,
+    #[serde(rename = "4")]
+    pub frame4: ParticipantFrame,
+    #[serde(rename = "5")]
+    pub frame5: ParticipantFrame,
+    #[serde(rename = "6")]
+    pub frame6: ParticipantFrame,
+    #[serde(rename = "7")]
+    pub frame7: ParticipantFrame,
+    #[serde(rename = "8")]
+    pub frame8: ParticipantFrame,
+    #[serde(rename = "9")]
+    pub frame9: ParticipantFrame,
+    #[serde(rename = "10")]
+    pub frame10: ParticipantFrame,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ParticipantFrame {
+    pub champion_stats: ChampionStats,
+    pub current_gold: i64,
+    pub damage_stats: DamageStats,
+    pub gold_per_second: i64,
+    pub jungle_minions_killed: i64,
+    pub level: i64,
+    pub minions_killed: i64,
+    pub participant_id: i64,
+    pub position: Position,
+    pub time_enemy_spent_controlled: i64,
+    pub total_gold: i64,
+    pub xp: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChampionStats {
+    pub ability_haste: i64,
+    pub ability_power: i64,
+    pub armor: i64,
+    pub armor_pen: i64,
+    pub armor_pen_percent: i64,
+    pub attack_damage: i64,
+    pub attack_speed: i64,
+    pub bonus_armor_pen_percent: i64,
+    pub bonus_magic_pen_percent: i64,
+    pub cc_reduction: i64,
+    pub cooldown_reduction: i64,
+    pub health: i64,
+    pub health_max: i64,
+    pub health_regen: i64,
+    pub lifesteal: i64,
+    pub magic_pen: i64,
+    pub magic_pen_percent: i64,
+    pub magic_resist: i64,
+    pub movement_speed: i64,
+    pub omnivamp: i64,
+    pub physical_vamp: i64,
+    pub power: i64,
+    pub power_max: i64,
+    pub power_regen: i64,
+    pub spell_vamp: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DamageStats {
+    pub magic_damage_done: i64,
+    pub magic_damage_done_to_champions: i64,
+    pub magic_damage_taken: i64,
+    pub physical_damage_done: i64,
+    pub physical_damage_done_to_champions: i64,
+    pub physical_damage_taken: i64,
+    pub total_damage_done: i64,
+    pub total_damage_done_to_champions: i64,
+    pub total_damage_taken: i64,
+    pub true_damage_done: i64,
+    pub true_damage_done_to_champions: i64,
+    pub true_damage_taken: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TimelineParticipant {
+    pub participant_id: i64,
+    pub puuid: String,
+}
+
 #[derive(Debug)]
 pub struct ByPUUIDOptions {
     pub start_time: Option<u64>,
@@ -379,6 +574,26 @@ impl MatchV5 {
             Err(err) => Err(Error::Message(err.to_string())),
         }
     }
+
+    pub fn match_timeline(&self, match_id: &str) -> Result<MatchTimeline, Error> {
+        let url = format!("{}/{}/timeline", self.endpoint, match_id);
+        let request = self.client.get(&url);
+        let response = request.send();
+        if let Err(error) = response {
+            return Err(Error::new_message(&format!("Failed:\n{}", error)));
+        }
+
+        let response = response.unwrap();
+
+        if response.status() != StatusCode::OK {
+            return Err(Error::StatusCode(response.status()));
+        }
+
+        match response.json::<MatchTimeline>() {
+            Ok(val) => Ok(val),
+            Err(err) => Err(Error::Message(err.to_string())),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -416,6 +631,17 @@ mod tests {
         let region = Region::AMERICAS;
         let api = MatchV5::new(&key, region);
         let res = api.match_info(&match_id).unwrap();
+        println!("{:#?}", res);
+    }
+
+    #[test]
+    fn test_get_match_timeline() {
+        dotenv().ok();
+        let key = var("API_KEY").unwrap();
+        let match_id = var("MATCH_ID").unwrap();
+        let region = Region::AMERICAS;
+        let api = MatchV5::new(&key, region);
+        let res = api.match_timeline(&match_id).unwrap();
         println!("{:#?}", res);
     }
 }
